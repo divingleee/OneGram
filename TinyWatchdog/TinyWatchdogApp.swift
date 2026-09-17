cb1dd9f9c56b3bbe26a8cd69d02db17130c7e9d7
lbq <divinglee@163.com>
2026-09-17 15:05:00 +0800

    Initial Commit

---
+++ TinyWatchdog/TinyWatchdogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyWatchdogApp.swift
+//  TinyWatchdog
+//
+//  Created by bbq on 2026/9/17.
+//
+
+import SwiftUI
+
+@main
+struct TinyWatchdogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
